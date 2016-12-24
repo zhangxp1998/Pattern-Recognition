1bad9a2d1adadd65ac432bd5b6cf12ee63b1fe3d
zhangxp1998 <zhangxp1998@gmail.com>
2016-12-24 08:56:13 -0500

    Add test client, fix bugs in fast algorithm

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,27 +1,30 @@
 import java.util.Arrays;
 import java.util.Comparator;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
 public class FastCollinearPoints
 {
 	private LineSegment lines[];
 
-	private Point min(Point p1, Point p2)
-	{
-		if (p1.compareTo(p2) < 0)
-			return p1;
-		else
-			return p2;
-	}
-
-	private Point max(Point p1, Point p2)
-	{
-		if (p1.compareTo(p2) > 0)
-			return p1;
-		else
-			return p2;
-	}
+	// private Point min(Point p1, Point p2)
+	// {
+	// if (p1.compareTo(p2) < 0)
+	// return p1;
+	// else
+	// return p2;
+	// }
+	//
+	// private Point max(Point p1, Point p2)
+	// {
+	// if (p1.compareTo(p2) > 0)
+	// return p1;
+	// else
+	// return p2;
+	// }
 
 	public FastCollinearPoints(Point[] points) // finds all line segments
 												// containing 4 points
@@ -36,21 +39,38 @@ public class FastCollinearPoints
 		for (Point p : old)
 		{
 			Comparator<Point> order = p.slopeOrder();
-			Arrays.sort(points, order);
-			int start = 0;
-			double startSlope = points[0].slopeTo(p);
-			for (int i = 1; i < points.length; i++)
+			Point[] copy = points.clone();
+			Arrays.sort(copy, order);
+			int start = 1;
+			double startSlope = copy[1].slopeTo(p);
+			for (int i = 2; i < copy.length; i++)
 			{
-				if (points[i].slopeTo(p) != startSlope)
+				if (copy[i].slopeTo(p) != startSlope)
 				{
 					if (i - start >= 3)
 					{
-						stack.push(new LineSegment(max(points[start], p), min(points[i - 1], p)));
-						start = i;
-						startSlope = points[i].slopeTo(p);
+						
+						Point min = copy[start];
+						Point max = copy[i - 1];
+						if (p.compareTo(min) < 0)
+						{
+							stack.push(new LineSegment(p, max));
+						}
 					}
+					start = i;
+					startSlope = copy[i].slopeTo(p);
 				}
 			}
+			if(copy.length - start >= 3)
+			{
+				Point min = copy[start];
+				Point max = copy[copy.length - 1];
+				if (p.compareTo(min) < 0)
+				{
+					stack.push(new LineSegment(p, max));
+				}
+			}
+			
 		}
 
 		lines = new LineSegment[stack.size()];
@@ -68,5 +88,38 @@ public class FastCollinearPoints
 	public LineSegment[] segments() // the line segments
 	{
 		return lines.clone();
+	}
+
+	public static void main(String[] args)
+	{
+		// read the n points from a file
+		In in = new In(args[0]);
+		int n = in.readInt();
+		Point[] points = new Point[n];
+		for (int i = 0; i < n; i++)
+		{
+			int x = in.readInt();
+			int y = in.readInt();
+			points[i] = new Point(x, y);
+		}
+
+		// draw the points
+		StdDraw.enableDoubleBuffering();
+		StdDraw.setXscale(0, 32768);
+		StdDraw.setYscale(0, 32768);
+		for (Point p : points)
+		{
+			p.draw();
+		}
+		StdDraw.show();
+
+		// print and draw the line segments
+		FastCollinearPoints collinear = new FastCollinearPoints(points);
+		for (LineSegment segment : collinear.segments())
+		{
+			StdOut.println(segment);
+			segment.draw();
+		}
+		StdDraw.show();
 	}
 }
